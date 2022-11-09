40cd343d0d5ef86cf6a7ce49b25961e815c1d635
Flori O <floroldani@hotmail.com>
2022-11-09 16:52:49 -0300

    cambio de estilos en HeadComponent

--- components/Characters/Characters.jsx
+++ components/Characters/Characters.jsx
@@ -4,7 +4,8 @@ const Characters = ({id,name}) => {
 	
 	return (
 		<div className='w-60'>
-			<h3 className="font-sains text-xl text-center text-zinc-200 hover:text-2xl">{name}</h3>
+			<h3>{id}</h3>
+			<h3 className="font-sains text-xl text-center text-zinc-500 hover:text-2xl hover:text-zinc-300">{name}</h3>
 		</div>
 		)
 };
--- components/HeadComponent/HeadComponent.jsx
+++ components/HeadComponent/HeadComponent.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const HeadComponent = () => {
 	return (
-		<div className=" w-full h-56 relative z-8">
+		<div className=" w-full h-56 relative z-8 shadow-xl shadow-neutral-700/50">
 			<Image src="/fondoEstrellado.png" width={500} height={500} alt="" className="w-full h-full object-cover absolute mix-blend-overlay "/>
 			   <div className=" w-1/2 h-full flex flex-row  justify-center items-center">
 			  <h1 className="font-mono text-7xl text-center text-zinc-300">STAR WARS</h1>
