f6a8412ca44ed1ea215e6866fdcee308f3acb572
Flori O <floroldani@hotmail.com>
2022-11-09 18:48:44 -0300

    barrel de paginado

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 export * from './Characters';
 export * from './HeadComponent';
-export * from './NavBar'
-export * from './Paginated'
+export * from './NavBar';
+export * from './Paginated';
+export * from './Search';
