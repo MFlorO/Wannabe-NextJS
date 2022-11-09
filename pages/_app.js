6196ff9eb0ddb57ede9bcd28fd950c62a023f140
Flori O <floroldani@hotmail.com>
2022-11-09 14:46:53 -0300

    inicializacion y configuracion de tailwind

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import Head from "next/head";
+
+
+function MyApp({ Component, pageProps }) {
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function({ addUtilities, addComponents, e, config }) {
+      // Add your custom styles here
+    }),
+  ]
+}
