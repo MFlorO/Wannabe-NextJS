8dcc3f0e747a9df01bca4760dad88c2ef87f43a8
Flori O <floroldani@hotmail.com>
2022-11-09 18:48:33 -0300

    Arreglo de fix de paginado con el search

--- components/Paginated/Paginated.jsx
+++ components/Paginated/Paginated.jsx
@@ -3,30 +3,23 @@ import React from 'react';
 
 export default function Paginated({pagina, setPagina, maximo}){
 
-
-    function siguiente(){
-        setPagina(pagina + 10)  
-     }
+    const next = () => setPagina(pagina + 1)  //Pagina es mi pagina actual, que inicia en 0,  por eso le tengo que sumar 1
+    const previous = () => setPagina(pagina - 1) //Pagina es mi pagina actual. Si estoy en next quiere decir que no estoy en 0. Por esto tengo que restar 1 para volver a mi pagina anterior
     
-    function anterior(){
-       setPagina(pagina - 10)
-    }
-    
-    const maximoRedondeo = Math.ceil(maximo) 
-
     return(
-        <div className='flex justify-center items-center'>
+        <div className='h-16 mb-6 flex justify-between items-center'>
+           
             <div>
-              <button onClick={anterior} 
-              className=" my-7 font-sains text-xl text-center text-zinc-200 underline decoration-soli"> 
+              <button onClick={previous} disabled={pagina === 0}
+              className="bg-black hover:bg-zinc-800 font-sains text-zinc-200 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> 
               PREVIOUS  </button>
             </div>
-            
-            <p>  {setPagina(pagina)} de {maximoRedondeo}</p>
 
+            <p className="mx-6 font-sains text-md text-center text-zinc-200">PAGINA: {pagina + 1 }</p>
+          
             <div>
-              <button onClick={siguiente}
-              className=" my-7 font-sains text-xl text-center text-zinc-200 underline decoration-soli"> 
+              <button onClick={next} disabled={pagina === maximo}
+               className="bg-black hover:bg-zinc-800 font-sains text-zinc-200 font-semibold py-2 px-6 border border-gray-400 rounded shadow"> 
               NEXT </button>
             </div>
         </div>
